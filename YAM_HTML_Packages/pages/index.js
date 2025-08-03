71be3c1074315d3be7276fb0cdfc3a0213b131cb
Kelvin Kimutai <kiruikevinkim@gmail.com>
2025-08-03 15:41:49 +0300

    added git ignore file

--- YAM_HTML_Packages/pages/index.js
+++ YAM_HTML_Packages/pages/index.js
@@ -1,27 +1,26 @@
 import Head from 'next/head'
 import useTranslation from '../components/ultils/useTranslation'
 import Header from '../components/Header'
-import SectionSlideshowIndex1 from '../components/SectionSlideshowIndex1'
-import SectionCategoriesSlider from '../components/SectionCategoriesSlider'
-import SectionProductTabDeal from '../components/SectionProductTabDeal'
-import SectionBrandListing from '../components/SectionBrandListing'
+import SectionSlideshowIndex2 from '../components/SectionSlideshowIndex2'
+import SectionProductTabDeal2 from '../components/SectionProductTabDeal2'
 import SectionTwoBanners from '../components/SectionTwoBanners'
 import SectionBannersBrand from '../components/SectionBannersBrand'
 import SectionProductSlider from '../components/SectionProductSlider'
 import SectionProductGrid from '../components/SectionProductGrid'
+import SectionBannersCaption from '../components/SectionBannersCaption'
 import Footer from '../components/Footer'
 import ExtNewsletterPopup from '../components/ExtNewsletterPopup'
 import ExtCookiesBar from '../components/ExtCookiesBar'
 import ExtNotification from '../components/ExtNotification'
 
-import { DataIndexBanners } from '../components/data/DataIndexBanners';
+import { DataIndexBanners2 } from '../components/data/DataIndexBanners2';
 import { DataIndexBannersBrand } from '../components/data/DataIndexBannersBrand';
 
-export default function Home() {
+export default function Home2() {
   if (typeof window !== 'undefined') {
     document.body.className = "";
     document.body.classList.add('template-index');
-    document.body.classList.add('yam-react-1');
+    document.body.classList.add('yam-react-2');
   }
   const { t } = useTranslation();
   return (
@@ -34,41 +33,43 @@ export default function Home() {
 
       <Header />
       <main>
-        <SectionSlideshowIndex1 />
-        <SectionCategoriesSlider />
-        <SectionProductTabDeal />
-        <SectionBrandListing />
+        <SectionSlideshowIndex2 />
+        <SectionProductTabDeal2 />
         <SectionTwoBanners
-          banner1={DataIndexBanners().img1}
-          banner2={DataIndexBanners().img2}
-          link1={DataIndexBanners().img1_link}
-          link2={DataIndexBanners().img2_link} />
+          banner1={DataIndexBanners2().img1}
+          banner2={DataIndexBanners2().img2}
+          link1={DataIndexBanners2().img1_link}
+          link2={DataIndexBanners2().img2_link} />
+        <SectionProductGrid titlecenter={true} />
         <SectionBannersBrand
           data1={DataIndexBannersBrand().data1}
-          data2={DataIndexBannersBrand().data2} />
+          data2={DataIndexBannersBrand().data2}
+          bannerPosition={'right'} />
         <SectionTwoBanners
-          banner1={DataIndexBanners().img3}
-          banner2={DataIndexBanners().img4}
-          link1={DataIndexBanners().img3_link}
-          link2={DataIndexBanners().img4_link} />
+          banner1={DataIndexBanners2().img3}
+          banner2={DataIndexBanners2().img4}
+          link1={DataIndexBanners2().img3_link}
+          link2={DataIndexBanners2().img4_link} />
         <SectionBannersBrand
           data1={DataIndexBannersBrand().data3}
-          data2={DataIndexBannersBrand().data4} />
+          data2={DataIndexBannersBrand().data4}
+          bannerPosition={'right'} />
         <SectionTwoBanners
-          banner1={DataIndexBanners().img5}
-          banner2={DataIndexBanners().img6}
-          link1={DataIndexBanners().img5_link}
-          link2={DataIndexBanners().img6_link} />
+          banner1={DataIndexBanners2().img5}
+          banner2={DataIndexBanners2().img6}
+          link1={DataIndexBanners2().img5_link}
+          link2={DataIndexBanners2().img6_link} />
+        <SectionProductSlider titlecenter={true} />
         <SectionBannersBrand
           data1={DataIndexBannersBrand().data5}
-          data2={DataIndexBannersBrand().data6} />
+          data2={DataIndexBannersBrand().data6}
+          bannerPosition={'right'} />
         <SectionTwoBanners
-          banner1={DataIndexBanners().img7}
-          banner2={DataIndexBanners().img8}
-          link1={DataIndexBanners().img7_link}
-          link2={DataIndexBanners().img8_link} />
-        <SectionProductSlider />
-        <SectionProductGrid />
+          banner1={DataIndexBanners2().img7}
+          banner2={DataIndexBanners2().img8}
+          link1={DataIndexBanners2().img7_link}
+          link2={DataIndexBanners2().img8_link} />
+        <SectionBannersCaption />
       </main>
       <Footer />
       <ExtNewsletterPopup />
