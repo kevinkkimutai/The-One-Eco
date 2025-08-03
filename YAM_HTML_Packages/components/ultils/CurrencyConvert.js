aa790f44c961caddf0e27a24bd4b611f0dd97bd7
Kelvin Kimutai <kiruikevinkim@gmail.com>
2025-08-03 15:52:22 +0300

    updated carousel overflow on mobile

--- YAM_HTML_Packages/components/ultils/CurrencyConvert.js
+++ YAM_HTML_Packages/components/ultils/CurrencyConvert.js
@@ -1,33 +1,28 @@
 import { useEffect, useState } from 'react';
-import CurrencyFormat from 'react-currency-format';
-import Axios from 'axios';
-import { useAtom } from 'jotai'
-import { storecurrency, storecurrencySymbol } from './Store'
-
 
 const CurrencyConvert = (props) => {
-    const [currency, setCurrency] = useAtom(storecurrency);
-    const [currencySymbol, setCurrencySymbol] = useAtom(storecurrencySymbol);
-    const [info, setInfo] = useState([]);
-    const [from, setFrom] = useState("usd");
-    const [options, setOptions] = useState([]);
     const [output, setOutput] = useState(props.amount);
-    useEffect(() => {
-        Axios.get(
-            `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`)
-            .then((res) => {
-                setInfo(res.data[from]);
-            });
-    }, [from]);
+    const currency = 'KSH';
+    const currencySymbol = 'KSh ';
 
     useEffect(() => {
-        setOptions(Object.keys(info));
-        setOutput((props.amount * info[currency]).toFixed(2));
-    }, [info, output, currency, props.amount]);
+        // Convert USD to KSH (using approximate exchange rate)
+        // You can update this rate or fetch from an API if needed
+        const usdToKshRate = 129.50; // Approximate rate, update as needed
+        setOutput((props.amount * usdToKshRate).toFixed(2));
+    }, [props.amount]);
+
+    // Format number with thousand separators
+    const formatCurrency = (value) => {
+        return parseFloat(value).toLocaleString('en-US', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+        });
+    };
 
     return (
         <span className={`money from-usd-${props.amount} to-${currency}`}>
-            <CurrencyFormat value={output} thousandSeparator={true} prefix={currencySymbol} displayType={'text'} />
+            {currencySymbol}{formatCurrency(output)}
             <span className='currency-name'>{currency}</span>
         </span>
     )
